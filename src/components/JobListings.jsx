d08f19a1e51a19f5ff4f642b9679e316b799c371
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-16 16:30:19 -0400

    fetch data

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,25 @@
+import {useState, useEffect} from 'react';
 import JobListing from './JobListing';
-import jobs from '../jobs.json';
 
 const JobListings = ({isHome=false}) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);        
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -11,11 +28,19 @@ const JobListings = ({isHome=false}) => {
           { isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {
-            jobListings.map((job) => (
-              <JobListing key={job.id} job={job} />
-            ))
+          { loading ? (
+            <h2>Loading</h2>
+            ) : (
+            <>
+              {
+                jobs.map((job) => (
+                  <JobListing key={job.id} job={job} />
+                ))
+              }
+            </>
+            ) 
           }
+
         </div>
       </div>
     </section>

00b60c800a946582c48299b837ac4f98a9038b96
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-16 19:04:41 -0400

    JobPage content

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,13 @@
 // import {useState, useEffect} from 'react'; 
 import {useParams, useLoaderData } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {FaArrowLeft, FaMapMarker} from 'react-icons/fa';
 
 const JobPage = () => {
   const {id} = useParams();
   const job = useLoaderData();
   
+  // COMMENT: jobLoader replaces useEffect
   // const [job, setJob] = useState(null);
   // const [loading, setLoading] = useState(true);
 
@@ -29,12 +32,12 @@ const JobPage = () => {
     <>
       <section>
         <div className="container m-auto py-6 px-6">
-          <a
-            href="/jobs.html"
+          <Link
+            to="/jobs"
             className="text-indigo-500 hover:text-indigo-600 flex items-center"
           >
-            <i className="fas fa-arrow-left mr-2"></i> Back to Job Listings
-          </a>
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
+          </Link>
         </div>
       </section>
 
@@ -43,13 +46,17 @@ const JobPage = () => {
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <main>
               <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-                <div className="text-gray-500 mb-4">Full-Time</div>
+                <div className="text-gray-500 mb-4">
+                  {job.type}
+                </div>
                 <h1 className="text-3xl font-bold mb-4">
-                  Senior React Developer
+                  {job.title}
                 </h1>
                 <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                  <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-                  <p className="text-orange-700">Boston, MA</p>
+                  <FaMapMarker className="text-lg text-orange-700 mr-2" />
+                  <p className="text-orange-700">
+                    {job.location}
+                  </p>
                 </div>
               </div>
   
@@ -57,47 +64,52 @@ const JobPage = () => {
                 <h3 className="text-indigo-800 text-lg font-bold mb-6">
                   Job Description
                 </h3>
-  
                 <p className="mb-4">
-                We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
+                  {job.description} 
                 </p>
-  
-                <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
-  
-                <p className="mb-4">$70k - $80K / Year</p>
+                <h3 className="text-indigo-800 text-lg font-bold mb-2">
+                  Salary
+                </h3>
+                <p className="mb-4">{job.salary} / Year</p>
               </div>
             </main>
   
             <aside>
               <div className="bg-white p-6 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold mb-6">Company Info</h3>
-  
-                <h2 className="text-2xl">NewTek Solutions</h2>
-  
+                <h3 className="text-xl font-bold mb-6">
+                  Company Info
+                </h3>
+                <h2 className="text-2xl">
+                  {job.company.name}
+                </h2>
                 <p className="my-2">
-                  NewTek Solutions is a leading technology company specializing in web development and digital solutions. We pride ourselves on delivering high-quality products and services to our clients while fostering a collaborative and innovative work environment.
+                  {job.company.description}
                 </p>
-  
                 <hr className="my-4" />
-  
-                <h3 className="text-xl">Contact Email:</h3>
-  
+                <h3 className="text-xl">
+                  Contact Email:
+                </h3>
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  contact@newteksolutions.com
+                  {job.company.contactEmail}
                 </p>
-  
-                <h3 className="text-xl">Contact Phone:</h3>
-  
-                <p className="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+                <h3 className="text-xl">
+                  Contact Phone:
+                </h3>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
               </div>
   
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                <a
-                  href="/add-job.html"
+                <h3 className="text-xl font-bold mb-6">
+                  Manage Job
+                </h3>
+                <Link
+                  to="/add-job"
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                  >Edit Job
-                </a>
+                >
+                  Edit Job
+                </Link>
                 <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
@@ -113,7 +125,6 @@ const JobPage = () => {
 const jobLoader = async ({params}) => {
   const res = await fetch(`/api/jobs/${params.id}`);
   const data = await res.json();
-  console.log(data);
   return data;
 }
 

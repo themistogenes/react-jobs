a906de53c03ed7f9c34d674e3a61845fe268225d
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-16 16:42:13 -0400

    conditional rendering of JobListings

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,8 +8,11 @@ const JobListings = ({isHome=false}) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome 
+        ? 'http://localhost:8000/jobs?_limit=3' 
+        : 'http://localhost:8000/jobs';
       try {
-        const res = await fetch('http://localhost:8000/jobs');
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);        
       } catch (error) {

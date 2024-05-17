e51c62d36e517e951ad67c4b7ddbc40590884e6f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-05-16 22:32:48 -0400

    EditJobPage and PUT request to edit job

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
@@ -20,6 +21,18 @@ const App = () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(newJob)
+    });
+    return;
+  }
+
+  // Update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
     });
     return;
   }
@@ -37,8 +50,17 @@ const App = () => {
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route 
+          path='/jobs/:id' 
+          element={<JobPage deleteJob={deleteJob} />} 
+          loader={jobLoader} 
+        />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route 
+          path='/edit-job/:id' 
+          element={<EditJobPage updateJobSubmit={updateJob} />} 
+          loader={jobLoader} 
+        />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -116,7 +116,7 @@ const JobPage = ({deleteJob}) => {
                   Manage Job
                 </h3>
                 <Link
-                  to="/add-job"
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
